2285b027688cae2418298a807631065725090268
Kholstein <40220033+Kholstein@users.noreply.github.com>
2018-11-06 19:39:51 -0500

    Week 1 Presentation build

---
+++ Assignment 6/Assets/Scripts/Kevin's Scripts/GameController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+
+	public PlayerController PC;
+
+	public GameObject[] CPlist;
+
+	void Update()
+	{
+		for(int i = 0; i < CPlist.Length; i++)
+        {
+            if (Vector3.Distance(PC.transform.position, this.transform.position) < 5)
+			{
+				//PC.Checkpointpos = CPlist[i].transform.position;
+			}
+        }
+	}
+
+}
