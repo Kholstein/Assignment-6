01ef027418fd41f047aef78f204f490eaa7c405a
Kholstein <40220033+Kholstein@users.noreply.github.com>
2018-11-11 19:43:36 -0500

    Week 2 stuff done & implemented

---
+++ Assignment 6/Assets/Scripts/Updated Scripts/LoadHighScores.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LoadHighScores : MonoBehaviour 
+{
+
+	public Text HighscoreText;
+
+	//public string HighscoreLevelName;
+
+	public void OnClick()
+	{
+		HighscoreText.text = string.Format("{0}:{1:00}", (int)PlayerPrefs.GetFloat("BestLapTime_1") / 60, (int)PlayerPrefs.GetFloat("BestLapTime_1") % 60);
+		//HighscoreText.text = string.Format("{0}:{1:00}", (int)PlayerPrefs.GetFloat("HighscoreLevelName") / 60, (int)PlayerPrefs.GetFloat("HighscoreLevelName") % 60);
+		//This is just set for the Debug Level, but with the commented out stuff this will work just fine for everything else.
+	}
+
+}
---
+++ Assignment 6/Assets/Scripts/Updated Scripts/PlayerController.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+public class PlayerController : MonoBehaviour {
+
+    //literally only changed how Checkpoints worked and how it registers the finish line
+    //you could totally ignore this script if you want to, or have already implemented my solution on the CP problem.
+
+    public float speed;
+    public Rigidbody rb;
+    public GameObject[] CPlist;
+    public int playerNumber = 0;
+
+    private Vector3 Checkpointpos;
+    [HideInInspector]
+    public bool Finish;
+
+    public Text DebugText;
+
+    public LapTimeManager LTM;
+
+    void Awake () 
+	{
+        LTM.PlayerCount += 1;
+        DebugText.text = "Race Begin";
+        Checkpointpos = transform.position;
+        Finish = false;
+        CPlist = GameObject.FindGameObjectsWithTag("Checkpoint");
+	}
+
+    void Update ()
+    {
+        Movement();
+        PlayerHealth();
+        if(Finish)
+        {
+            DebugText.text = "Race Finished";
+        }
+    }
+
+    void Movement()
+    {
+        float moveHorizontal = Input.GetAxis ("Horizontal");
+        float moveVertical = Input.GetAxis ("Vertical");
+
+        Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+
+        rb.AddForce (movement * speed);
+    }
+
+    void PlayerHealth()
+    {
+        if(transform.position.y <= -10)
+        {
+            transform.position = Checkpointpos;
+        }
+
+        for(int i = 0; i < CPlist.Length; i++)
+        {
+            if (Vector3.Distance(transform.position, CPlist[i].transform.position) < 5)
+            {
+                Checkpointpos = CPlist[i].transform.position;
+            }
+        }
+
+        if (Vector3.Distance(transform.position, GameObject.FindGameObjectWithTag("FinishLine").transform.position) < 5)
+        {
+            Finish = true;
+        }
+    }
+}
