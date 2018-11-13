864f2064a54aa4b5ed6495b099ffb686ee359ed8
adavidson617 <44651006+adavidson617@users.noreply.github.com>
2018-11-13 15:18:41 -0500

    Week 2 Saving high scores

---
+++ UnityProject/Assets/Scripts/Kevin's Scripts/CameraControl.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class CameraControl : MonoBehaviour {
+
+    public GameObject player;
+
+    private Vector3 offset;
+
+    void Start ()
+    {
+        offset = transform.position - player.transform.position;
+    }
+    
+    void LateUpdate ()
+    {
+        transform.position = player.transform.position + offset;
+    }
+}
