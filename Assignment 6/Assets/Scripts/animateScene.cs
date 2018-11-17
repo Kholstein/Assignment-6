2c7ea7d20cfe963add6718d7b1c98cf7c1ee3890
Kholstein <40220033+Kholstein@users.noreply.github.com>
2018-11-17 18:51:56 -0500

    Week 2 start

---
+++ Assignment 6/Assets/Scripts/animateScene.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class animateScene : MonoBehaviour {
+	Animator anim;
+
+	// Use this for initialization
+	void Start () {
+		anim = GetComponent <Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//debug
+		if (PlayerController.finishNumber == 1) {
+			anim.SetTrigger ("finishRace");
+		}
+		if (PlayerController.finishNumber == 4) {
+			anim.SetTrigger ("finishRace");
+		}
+	}
+}
