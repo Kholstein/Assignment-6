8954f6a71522a6dbc456c785dd25cc4a82c3458e
Kholstein <40220033+Kholstein@users.noreply.github.com>
2018-11-25 14:41:06 -0500

    Fixed Week 3 master branch error

---
+++ Assignment 6/Assets/Scripts/Zach's Scripts/powerUpPickUp.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class powerUpPickUp : MonoBehaviour
+{
+    public float duration = 4f;
+    public GameObject player; //A player
+    public GameObject pickUpEffect; // What particle effect happens when object is pickedup.
+    private AudioSource soundOrigin; // What the sound is coming from.
+    public AudioClip pickUpSound; // What sound plays when the powerup has been picked up.
+
+    public Text UI;
+
+
+
+    void Start()
+    {
+        player = GameObject.Find("");
+        soundOrigin = GetComponent<AudioSource> ();
+    }
+    void OnTriggerEnter(Collider other) // When a plyer walks into the powerup...
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            StartCoroutine( pickUp()); // ...call the method used to pickup the powerup
+            // other.gameObject.SetActive(false);
+        }
+    }
+
+    IEnumerator pickUp() // This method will give the player a random powerup and then deactivate itself.
+    {
+        //Instantiate(pickUpEffect, transform.position, transform.rotation); // Causes a specific particle effect to show when powerup is picked up
+
+        
+        soundOrigin.PlayOneShot(pickUpSound); // Play sound once
+
+        // List<"i"> = powerUpsList[Random.range(0, powerUpsList.Count)] // Select a random element from the list of powerups...
+
+
+        GetComponent<MeshRenderer>().enabled = false;
+        GetComponent<Collider>().enabled = false;
+
+        yield return new WaitForSeconds(duration); // Wait for this long...
+
+        UI.text = "powerup expire";
+
+        gameObject.SetActive(false); // ... until deactivating the powerup
+
+    }
+}
---
+++ Assignment 6/Assets/Scripts/playerSpawner.cs
@@ -0,0 +1,106 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class playerSpawner : MonoBehaviour {
+	public GameObject player;
+	public Transform[] spawnPoints;
+	public Camera cam;
+	private int playerNumber;
+	private int countPlayer = 0;
+	private int countPlayerNumber = 1;
+	private int camNumber = 0;
+	private int countCamNumber = 1;
+	private int setCamNumCount= 0;
+	public int playerCount = 4;
+
+	//pause script variables
+	public static bool GameIsPaused = false;
+
+	//public int setPlayerNumber;
+	// Use this for initialization
+	void Start () {
+		spawnPlayers ();
+	}
+
+	void Update ()
+	{
+		if (Input.GetKeyDown(KeyCode.Escape) || Input.GetKeyDown("joystick button 7"))
+		{
+			if (GameIsPaused)
+			{
+				Resume();
+			}
+			else
+			{
+				Pause();
+			}
+		}
+	}
+
+	void spawnPlayers ()
+	{
+
+		for (int i = 1; i <= playerCount; i++) {
+			GameObject spawnedChild = Instantiate (player, spawnPoints [countPlayer].position, spawnPoints [countPlayer].rotation);
+			Camera childCam = spawnedChild.GetComponentInChildren<Camera> ();
+			countPlayer++;
+			if (i == 1) {
+				childCam.gameObject.tag = "camera1";
+				childCam.rect = new Rect (0.0f, 0.5f, 0.5f, 0.5f);
+			}
+			else if (i == 2) {
+				childCam.gameObject.tag = "camera2";
+				childCam.rect = new Rect (0.5f, 0.5f, 0.5f, 0.5f);
+			}
+			else if (i == 3) {
+				childCam.gameObject.tag = "camera3";
+				childCam.rect = new Rect (0.0f, 0.0f, 0.5f, 0.5f);
+			}
+			else if (i == 4) {
+				childCam.gameObject.tag = "camera4";
+				childCam.rect = new Rect (0.5f, 0.0f, 0.5f, 0.5f);
+			}
+		}
+	}
+
+	public int setPlayerNumber()
+	{
+		playerNumber = countPlayerNumber++;
+		return playerNumber;
+	}
+
+	public int setCamNumber()
+	{
+		camNumber = countCamNumber++;
+		return camNumber;
+	}
+
+	public int setCamRectNumber()
+	{
+		setCamNumCount++;
+		return setCamNumCount;
+	}
+
+	public void Resume ()
+	{
+		SceneManager.UnloadSceneAsync("PauseMenu");
+		Time.timeScale = 1f;
+		GameIsPaused = false;
+	}
+
+	public void Pause ()
+	{
+		SceneManager.LoadScene("PauseMenu", LoadSceneMode.Additive);
+		Time.timeScale = 0f;
+		GameIsPaused = true;
+	}
+
+	//	//SceneManager.LoadScene("LevelSelect");
+	//	public void MenuReturn ()
+	//	{
+	//		Debug.Log ("Retunring to Menu...");
+	//		SceneManager.LoadScene("MainMenu");
+	//
+	//	}
+}
