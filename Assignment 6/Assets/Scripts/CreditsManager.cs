b3ce0fea4e477640729f05f039d9a58cd8bcc3e1
Kholstein <40220033+Kholstein@users.noreply.github.com>
2018-11-24 20:19:16 -0500

    Week 12 push

---
+++ Assignment 6/Assets/Scripts/CreditsManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreditsManager : MonoBehaviour 
+{
+	// takes 2 canvas objects and basically is like a light switch. Has an off/on button(s). Idk how you will implement it, but do what you wanna do.
+	public GameObject Credits;
+	public GameObject Menu;
+
+	public void ShowCredits()
+	{
+		Credits.SetActive(true);
+		Menu.SetActive(false);
+	}
+
+	public void HideCredits()
+	{
+		Credits.SetActive(false);
+		Menu.SetActive(true);
+	}
+
+}
---
+++ Assignment 6/Assets/Scripts/HighScoresManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HighScoresManager : MonoBehaviour {
+
+    public GameObject highScores;
+    public GameObject Menu;
+
+    public void ShowHighScores()
+    {
+        highScores.SetActive(true);
+        Menu.SetActive(false);
+    }
+
+    public void HideHighScores()
+    {
+        highScores.SetActive(false);
+        Menu.SetActive(true);
+    }
+}
