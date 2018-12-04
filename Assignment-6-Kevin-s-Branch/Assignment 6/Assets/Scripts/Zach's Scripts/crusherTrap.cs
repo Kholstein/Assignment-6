fc3b2c54e782061bdc815083bdd197d8ded0114f
Kholstein <40220033+Kholstein@users.noreply.github.com>
2018-12-04 13:47:17 -0500

    Kevin/Zach combine commit

---
+++ Assignment-6-Kevin-s-Branch/Assignment 6/Assets/Scripts/Zach's Scripts/crusherTrap.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+public class crusherTrap : MonoBehaviour
+{
+    public Transform StopPoint;
+    private Vector3 StartPoint;
+    private Vector3 StopPosition;
+    public float UpDownLoopTime = 5f;
+
+    public Transform respawnPoint;
+
+
+    void Start()
+    {
+        StartPoint = transform.position;
+        StopPosition = StopPoint.position;
+    }
+
+    void Update()
+    {
+        transform.position = Vector3.Lerp(StartPoint, StopPosition, Mathf.SmoothStep(0f, 1f, Mathf.PingPong(Time.time / UpDownLoopTime, 1f)));
+    }
+
+    private void OnTriggerEnter(Collider player)
+    {
+        player.transform.position = respawnPoint.transform.position;
+    }
+}
---
+++ Assignment-6-Kevin-s-Branch/Assignment 6/Assets/Scripts/Zach's Scripts/lavaTrapScript.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class lavaTrapScript: MonoBehaviour
+{
+    public float BlinkForThisManySeconds;
+    public bool Waited;
+    public AudioClip SoundToPlay;
+    AudioSource audio;
+
+    [SerializeField] private Transform respawnPoint;
+
+    void OnTriggerEnter(Collider player) // When a player collides...
+    {
+        audio = GetComponent<AudioSource>();
+        audio.PlayOneShot(SoundToPlay); // Play an audio sound once.
+
+        StartCoroutine(BlinkingDamage()); // Wait for X seconds and blink.
+
+        if (Waited == true) // After waiting for X seconds...
+        {
+            player.transform.position = respawnPoint.transform.position; // Respawn the player.
+            Debug.Log("Wait completed!");
+        }
+        else
+        {
+            Debug.Log("Wait not completed!");
+        }
+
+
+    }
+    IEnumerator BlinkingDamage()
+    {
+        yield return new WaitForSeconds(BlinkForThisManySeconds); // Wait for X seconds.
+        /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        ///////////////ERROR - There is no 'MeshRenderer' attached to the "playerManager" game object, but a script is trying to access it.//////////////
+        /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        /*for(int BlinkCount = 0; BlinkCount < 5; BlinkCount++) //Make the player 'blink' for 5 seconds. 
+        {
+            Debug.Log("Counting!");
+            player.GetComponent<MeshRenderer>().enabled = true;
+            player.GetComponent<MeshRenderer>().enabled = false;
+    }*/
+
+        Waited = true; // Turn bool value to true.
+
+    }
+}
