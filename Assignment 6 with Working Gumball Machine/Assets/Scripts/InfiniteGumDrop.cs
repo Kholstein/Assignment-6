90c746f0ecd5387a834c688a04464c46324f29f5
Kholstein <40220033+Kholstein@users.noreply.github.com>
2018-12-10 00:00:31 -0500

    Week 15 stuff done.

---
+++ Assignment 6 with Working Gumball Machine/Assets/Scripts/InfiniteGumDrop.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InfiniteGumDrop : MonoBehaviour {
+
+	public GameObject Gumball;
+	public Material[] matlist;
+
+	private Vector3 SpawnPos;
+	public float timer;
+	private bool spawned = false;
+
+	private GameObject GumSpawn;
+	void Start () 
+	{
+		SpawnPos = transform.position;
+		this.gameObject.GetComponent<Renderer>().material = matlist[Random.Range(0,4)];
+		timer = 0;
+	}
+	
+	
+	void Update () 
+	{
+		timer += Time.deltaTime;
+		if (timer > 3 & !spawned)
+		{
+			GumSpawn = Instantiate(Gumball, SpawnPos, transform.rotation);
+			spawned = true;
+		}
+	}
+}
