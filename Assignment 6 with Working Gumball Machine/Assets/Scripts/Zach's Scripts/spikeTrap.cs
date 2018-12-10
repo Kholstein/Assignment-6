dcd261437d5ee4cbf67da20a2b9e23369b0e78db
RoseWoodKitty <cinos.wolf@gmail.com>
2018-12-09 22:14:22 -0500

    Updated Build from my end
    a build updated with my tasks as well as possibly

---
+++ Assignment 6 with Working Gumball Machine/Assets/Scripts/Zach's Scripts/spikeTrap.cs
@@ -0,0 +1,24 @@
+﻿/*using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spikeTrap : MonoBehaviour
+{
+    public float pushBackForce;
+    public AudioClip SoundToPlay;
+    AudioSource audio;
+
+    [SerializeField] public player;
+
+
+        private void OnTriggerEnter(Collider player)
+    {
+        audio = GetComponent<AudioSource>();
+        audio.PlayOneShot(SoundToPlay); // Play an audio sound once.
+
+        Vector3 dir = player.contacts[0].point - transform.position;
+        dir = -dir.normalized;
+        GetComponent<Rigidbody>().AddForce(dir * pushBackForce);
+    }
+}
+*/
